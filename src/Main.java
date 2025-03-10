7d4d85cb3d97dfc6d4130ff4158ddb3a75e908a2
Joao Carlos Luzardo de Aragao <joao.aragao@mpt.mp.br>
2025-03-10 19:45:59 -0300

    Segunda aula

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        int valor1 = 10;
-        int valor2 = 5;
+        System.out.println("Esse é o ScreenMatch");
+        System.out.println("Filme: Top Gun: Maverik");
 
-        System.out.println(Soma.somaValor(valor1, valor2));
-        System.out.println(Subtracao.valorSubtracao(valor1, valor2));
+        int ano = 2022;
+        System.out.println("Ano de Lançamento: " + ano);
+
+        boolean incluidoNoPlano = false;
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse = "Filme de Aventura com o Tom Cruise";
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+
+
+
+        System.out.println("Classificação: " + classificacao);
+
+
+
     }
 }
